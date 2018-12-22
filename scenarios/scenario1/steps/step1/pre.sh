f8e5444a986f4ae161f5de4c89067fe38ed52cdb
heming6666 <1040023934@qq.com>
2018-12-22 23:54:02 +0800

    add

--- scenarios/scenario1/steps/step1/pre.sh
+++ scenarios/scenario1/steps/step1/pre.sh
@@ -1,2 +1,4 @@
-mkdir -p /home/scrapbook/tutorial
-cd /home/scrapbook/tutorial
+mkdir -p /home/scrapbook/tutorial/1
+cd /home/scrapbook/tutorial/1
+touch hello-world.js
+echo "console.log(\"Hello World\");" >> hello-world.js
